a8dae7a2ec25b0d08a588bc1a9ed67cd6b07beaf
Ayça Albayrak <aycalbyrkk@gmail.com>
2025-09-17 19:51:22 +0300

    Delete apartmanyonetim/APTYONETIM directory

--- apartmanyonetim/APTYONETIM/src/AktifDurum.java
+++
@@ -1,6 +0,0 @@
-public class AktifDurum implements KullaniciDurumu {
-    @Override
-    public void durumGuncelle(Kullanici kullanici) {
-        System.out.println(kullanici.getAd() + " artık aktif durumunda.");
-    }
-}
--- apartmanyonetim/APTYONETIM/src/KullaniciDurumu.java
+++
@@ -1,3 +0,0 @@
-public interface KullaniciDurumu {
-    void durumGuncelle(Kullanici kullanici);
-}
